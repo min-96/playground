772d3bb75c74fee3361792770b9870cdcf4c3e2d
min-96 <ajdcnd131@naver.com>
2021-07-28 03:43:09 +0900

    mysql 연동라이브러리 추가, 롬북 라이브러리 추가 , 롬북 어노테이션 테스트

---
+++ src/main/java/org/hdcd/domain/Board.java
@@ -0,0 +1,32 @@
+package org.hdcd.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor //인자가 없는 기본 생성자를 생성
+@RequiredArgsConstructor //nonnull이 적용된 필드값이나 final로 선언된 필드값만 인자로 받는 생성자 생성
+@EqualsAndHashCode(of="boardNo")
+//@ToString
+@ToString(exclude="regDate") // regDate 제외한 toString 메소드 생성
+public class Board {
+	private int boardNo;
+	@NonNull
+	private String title;
+	@NonNull
+	private String content;
+	private String writer;
+	private LocalDateTime regDate;
+	
+
+}
---
+++ src/test/java/org/hdcd/LombokTests/LombokTests.java
@@ -0,0 +1,37 @@
+package org.hdcd.LombokTests;
+
+import org.hdcd.domain.Board;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LombokTests {
+	@Test
+	public void testNoArgsConstructor() {
+		Board board= new Board();
+		System.out.println(board);
+		
+	}
+	@Test
+	public void testRequirdArgsConstructor() {
+		Board board= new Board("테스트제목","테스트테스트");
+		System.out.println(board);
+		
+	}
+	@Test
+	public void testGetter() {
+		Board board= new Board();
+		System.out.println(board.getTitle());
+	}
+	@Test
+	public void testSetter() {
+		Board board= new Board();
+		board.setTitle("게시판제목");
+		System.out.println(board.getTitle());
+	}
+	@Test
+	public void testTostring() {
+		Board board= new Board();
+		System.out.println(board);
+	}
+}
