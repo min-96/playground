f288db404f4b30436093ed5af4848df29c4f6c14
min-96 <ajdcnd131@naver.com>
2021-07-28 21:40:19 +0900

    Thymeleaf / Logging /요청경로매핑

---
+++ src/main/java/org/hdcd/controller/BoardController.java
@@ -0,0 +1,47 @@
+package org.hdcd.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jdk.internal.org.jline.utils.Log;
+import lombok.extern.slf4j.Slf4j;
+@Controller
+@Slf4j
+//클래스 레벨 요청경로지정
+@RequestMapping("/board")
+public class BoardController {
+	
+	//private static final Logger logger=LoggerFactory.getLogger(BoardController.class);
+	
+	
+	//value속성에 요청경로를 설정한다
+	//@RequestMapping("/register")
+	@GetMapping("/register")
+	public String registerForm() {
+		log.info("registerForm");
+		return "board/register";
+	}
+	//@RequestMapping("/modify")
+	@PostMapping("/modify")
+	public String modifyForm() {
+		log.info("modifyForm");
+		return "board/modify";
+	}
+	@RequestMapping("/list")
+	public void listForm() {
+		log.info("listForm");
+	}
+	//경로 패턴 지정
+	@GetMapping("/read/{boardNo}")
+	public String read(@PathVariable int boardNo) {
+		log.info("readBoardNo: "+boardNo);
+		//경로가 변하므로 뷰 이름 지정
+		return "board/read";
+	}
+
+}
--- src/main/java/org/hdcd/controller/HomeController.java
+++ src/main/java/org/hdcd/controller/HomeController.java
@@ -11,14 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//자동으로 생기는 log라는 변수를 이용해서 로그를 출력할수있다
 @Controller
 public class HomeController {
-	private static final Logger logger =LoggerFactory.getLogger(HomeController.class);
+	//private static final Logger logger =LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String home(Locale locale,Model model) {
-		logger.info("Welcom home! The Client locale is {}.",locale);
-		
+		//logger.info("Welcom home! The Client locale is {}.",locale);
+		log.info("Welcom home! The client locale is" +locale+".");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.LONG,locale);
 		String formattedDate = dateFormat.format(date);
