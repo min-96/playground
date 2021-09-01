1e0b4a7989089d089a34ca5a67707a3567df694a
min96 <ajdcnd131@naver.com>
2021-09-02 05:31:37 +0900

    AOP 비지니스 메소드 테스트

--- src/main/java/org/hdcd/common/aop/ServiceLoggerAdvice.java
+++ src/main/java/org/hdcd/common/aop/ServiceLoggerAdvice.java
@@ -2,9 +2,8 @@ package org.hdcd.common.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -15,11 +14,12 @@ import java.util.Arrays;
 @Component
 //관심사모듈화클래스 지정
 @Aspect
+    //advice - 횡단 관심에 해당하는 공통 기능의 코드
 public class ServiceLoggerAdvice {
 
 
-    //JoinPoint-메소드가 호출되는 부분 또는 리턴되는부분
-    //advice - 횡단 관심에 해당하는 공통 기능의 코드
+
+    //JoinPoint-메소드가 호출되는 부분 또는 리턴되는부분 //비지니스메소드
     //Before - 조인포인트 전에 실행된다 예외가 발생하는 경우만 제외하고 항상 실행됨
     //org.hdcd.service아래의 패키지 경로의 BoardService 객체의 모든 메서드에 이 Aspect를 적용하겠다는 의미다.
     //포인트컷 표현식 execution 특정 조건에 의해 필터링된 조인포인트, 수많은 조인포인트 중에 특정 메소드에서만 횡단 공통기능을 수행시키기 위해서 사용한다.
@@ -37,4 +37,60 @@ public class ServiceLoggerAdvice {
         log.info("logRunning");
         log.info("logRunning:"+jp.getSignature());
     }
+
+    //조인포인트에서 예외가 발생했을때 실행된다.
+    @AfterThrowing(pointcut = "execution(* org.hdcd.service.BoardService*.*(..))",throwing = "e")
+    public void logException(JoinPoint jp,Exception e){
+        log.info("logException");
+        log.info("logException :" + jp.getSignature());
+        log.info("logException :"+e);
+
+    }
+    //조인포인트에서 처리가 완료된 후 실행 예외발생이나 정상종료 여부와 상관없이 실행됨
+    @After("execution(* org.hdcd.service.BoardService*.*(..))")
+    public void endLog(JoinPoint jp){
+        log.info("endLog");
+        log.info("endLog : "+ jp.getSignature());
+        log.info("endLog :" + Arrays.toString(jp.getArgs()));
+    }
+    //조인포인트 전후에 실행됨
+    @Around("execution(* org.hdcd.service.BoardService*.*(..))")
+    public Object timeLog(ProceedingJoinPoint pjp) throws Throwable {
+    //around 어드바이스 경우는 클라이언트 호출을 가로챈다 따라서 막바로 리턴을 해버리면
+        //비지니스메소드 자체가 실행이안 around 메소드에서 책임을 져야됨
+        //비지니스메소드에 대한 정보를 around 메소드가 알려면 ProceedongJoingPoint 객체
+        long startTime = System.currentTimeMillis();
+
+        log.info(Arrays.toString(pjp.getArgs()));
+
+
+        Object result = pjp.proceed();
+
+        long endTime = System.currentTimeMillis();
+
+        log.info(pjp.getSignature().getName()+":"+(endTime-startTime));
+
+        return result;
+    }
+
+    @Before("execution(* org.hdcd.service.BoardService*.*(..))")
+    public void log(JoinPoint jp){
+        //프락시가 입혀지기 전에 원본대상 객체를 가져옴
+        Object targetObject  = jp.getTarget();
+        log.info("targetObject : "+targetObject);
+
+        //프락시를 가져옴
+        Object thisObject = jp.getThis();
+
+        log.info("thisObject : "+ thisObject);
+
+        //인수를 가져옴
+
+        Object[] args = jp.getArgs();
+
+        log.info("args.legth=" + args.length);
+
+
+    }
+
 }
