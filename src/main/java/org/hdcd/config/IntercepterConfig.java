7e5ced7f8df0e1518a5ec50e7201aa7085bd3fc7
min96 <ajdcnd131@naver.com>
2021-09-03 11:22:24 +0900

    컨트롤러에 접근한느 로그를 저장하는 인터셉터 정의

--- src/main/java/org/hdcd/config/IntercepterConfig.java
+++ src/main/java/org/hdcd/config/IntercepterConfig.java
@@ -18,7 +18,10 @@ public class IntercepterConfig implements WebMvcConfigurer {
 
     public void addInterceptors(InterceptorRegistry registry){
         //원하는 URI에 적절한 패턴을 적용하여 인터셉터를 지정한다
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/login");
+      //  registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/login");
+
+
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/resource/**");
 
         WebMvcConfigurer.super.addInterceptors(registry);
     }
---
+++ src/main/java/org/hdcd/interceptor/AccessInterceptor.java
@@ -0,0 +1,40 @@
+package org.hdcd.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+
+//접근 로그 저장 인터셉터
+@Slf4j
+public class AccessInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String requestUrl = request.getRequestURI();
+
+        log.info("requestURL : "+requestUrl);
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        Class clazz= method.getDeclaringClass();
+
+        String className = clazz.getName();
+        String classSimpleName = clazz.getSimpleName();
+        String methodName = method.getName();
+
+        log.info("[ACESS CINTROLLER]" + className+"."+methodName);
+    }
+}
