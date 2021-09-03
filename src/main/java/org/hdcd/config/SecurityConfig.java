550d74a9de5d474ec200d7c4b926b3ef81c3da9f
min96 <ajdcnd131@naver.com>
2021-09-03 15:18:57 +0900

    사용자 정의 접근거부처리

--- src/main/java/org/hdcd/config/SecurityConfig.java
+++ src/main/java/org/hdcd/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package org.hdcd.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.hdcd.config.security.CustomerAccessDeniedHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @EnableWebSecurity
 @Slf4j
@@ -37,13 +40,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //폼 기반 인증기능을 사용한다.
         http.formLogin();
     //접근 거부 처리자의 URI를 지정
-       http.exceptionHandling()
-                .accessDeniedPage("/accessError");
+//       http.exceptionHandling()
+//                .accessDeniedPage("/accessError");
 
         //등록한 CustomAccressDeniendHandler를 접근거부 처리자로 지정
+        http.exceptionHandling()
+                .accessDeniedHandler(createAccessDeniedHandler());
 
 
-
+    }
+    //접근거부가 발생한 상황에 단순 메세지 처리 이상의 다양한 처리를 하고싶다면
+    //AccessDeniedHandler 직접구현해야됨.
+    @Bean
+    public AccessDeniedHandler createAccessDeniedHandler() {
+        return new CustomerAccessDeniedHandler();
     }
 
     //지정된 아이디와 패스워드로 로그인이 가능하게 설정한다
---
+++ src/main/java/org/hdcd/config/security/CustomerAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package org.hdcd.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+//사용자가 정의한 접근 거부 처리자
+@Slf4j
+public class CustomerAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        log.info("handle");
+
+        httpServletResponse.sendRedirect("/accessError");
+    }
+}
